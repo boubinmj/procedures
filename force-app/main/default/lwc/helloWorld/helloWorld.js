6baf4ef29bd57d0f2d23b7d4c4aed074f25aad56
Matthew Boubin <mjb9353@nyu.edu>
2024-12-17 15:11:03 -0500

    added account search feature

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,7 +1,30 @@
-import { LightningElement } from 'lwc';
+import { LightningElement , track, wire} from 'lwc';
+import searchAccounts from '@salesforce/apex/AccountSearchController.searchAccounts';
 export default class HelloWorld extends LightningElement {
-        greeting = 'Matt';
-        changeHandler(event) {
-        this.greeting = event.target.value;
+        @track searchKey = '';
+        @track accounts = [];
+        @track error;
+
+        // Event to handle user input
+        handleSearchInput(event) {
+                this.searchKey = event.target.value;
+                if (this.searchKey.length >= 2) {
+                this.performSearch();
+                } else {
+                this.accounts = []; // Clear results if input is too short
+                }
+        }
+
+        // Call Apex to search for accounts
+        performSearch() {
+                searchAccounts({ searchKey: this.searchKey })
+                .then(result => {
+                        this.accounts = result;
+                        this.error = undefined;
+                })
+                .catch(error => {
+                        this.error = error;
+                        this.accounts = [];
+                });
         }
 }
